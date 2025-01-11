4321d808bce55624d80f4d6d61a82293c7338731
badnumberr <ymniychel000@gmail.com>
2025-01-11 15:27:04 +0500

    character

--- mouse cursor/main.py
+++ mouse cursor/main.py
@@ -1,27 +1,35 @@
 import pygame
 
 pygame.init()
-w, h = 400, 400
-s = pygame.display.set_mode((w, h))
-pygame.display.set_caption('Желтый круг')
-circle = 0
-pos = (0, 0)
-gr = False
-v = 10
-clock = pygame.time.Clock()
-run = True
-while run:
+
+w = 300
+h = 300
+screen = pygame.display.set_mode((w, h))
+pygame.display.set_caption("герой двигается")
+
+size = 100
+speed = 10
+x = w // 2 - size // 2
+y = h - size - 10
+
+image = pygame.image.load("data/creature.png")
+image = pygame.transform.scale(image, (size, size))
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = event.pos
-            circle = 0
-            gr = True
-    s.fill('blue')
-    if gr:
-        circle += v * (clock.get_time() / 1000)
-    pygame.draw.circle(s, 'yellow', pos, int(circle))
+            running = False
+    k = pygame.key.get_pressed()
+    if k[pygame.K_LEFT]:
+        x -= speed
+    if k[pygame.K_RIGHT]:
+        x += speed
+    if k[pygame.K_UP]:
+        y -= speed
+    if k[pygame.K_DOWN]:
+        y += speed
+    screen.fill('white')
+    screen.blit(image, (x, y))
     pygame.display.flip()
-    clock.tick(60)
+    pygame.time.delay(30)
 pygame.quit()
